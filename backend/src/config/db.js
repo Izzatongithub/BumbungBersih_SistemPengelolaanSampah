26ff48559e09ac23198875358c97f31dff887f2b
izzatongithub <izzatnazhiefa24@gmail.com>
2026-06-29 20:58:13 +0800

    debug

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,5 +1,12 @@
 const mysql = require("mysql2/promise");
 
+console.log({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  pass: process.env.DB_PASS ? "ADA" : "KOSONG",
+  db: process.env.DB_NAME,
+});
+
 const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
